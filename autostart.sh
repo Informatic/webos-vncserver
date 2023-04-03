4f932cf65fed5187d6de15f93f37c558f340bd95
Mariotaku <mariotaku.lee@gmail.com>
2023-04-03 18:24:04 +0900

    fixed timeout command for pre-webOS 7

--- autostart.sh
+++ autostart.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-timeout 5 luna-send -n 1 'luna://org.webosbrew.vncserver.service/start' '{"autostart": true}'
+
+COMMAND='luna-send -n 1 luna://org.webosbrew.vncserver.service/start {"autostart": true}'
+
+timeout 5 $COMMAND
+# On pre-webOS 7, busybox uses non-standard timeout arguments. We need to retry for that.
+if [ $? -eq 127 ]; then
+    timeout -t 5 $COMMAND
+fi
