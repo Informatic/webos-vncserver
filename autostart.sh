719dc0593aae2c8771af870092983464ca907155
Mariotaku <mariotaku.lee@gmail.com>
2024-02-06 18:00:57 +0900

    Use timeout feature in luna-send

--- autostart.sh
+++ autostart.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 
-COMMAND='luna-send -n 1 luna://org.webosbrew.vncserver.service/start {"autostart": true}'
-
-timeout 5 $COMMAND
-# On pre-webOS 7, busybox uses non-standard timeout arguments. We need to retry for that.
-if [ $? -eq 127 ]; then
-    timeout -t 5 $COMMAND
-fi
+luna-send -n 1 -w 5000 luna://org.webosbrew.vncserver.service/start '{"autostart": true}'
